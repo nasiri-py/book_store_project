2450efa61532132d9b7f2b45e5d40dbb9cbce4f3
sinanasiripy <sinanasiri.py@gmail.com>
2022-04-28 22:02:25 +0430

    add media config to project

--- config/urls.py
+++ config/urls.py
@@ -17,6 +17,9 @@ from django.contrib import admin
 from django.urls import path, include
 # from django.views.generic.base import TemplateView
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path('home/', TemplateView.as_view(template_name='home.html'), name='home'),
@@ -24,4 +27,4 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('accounts.urls')),
     path('books/', include('books.urls'))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
